81c867d933a407b337c1f2ba9c02ff8f923ac1c2
Artem Lakomov <a.lakomov@hotmail.com>
2014-09-17 20:55:59 +0300

    updated plugin

--- src/wp8/Pedometer.cs
+++ src/wp8/Pedometer.cs
@@ -14,11 +14,11 @@ namespace Cordova.Extension.Commands
 {
     public class Pedometer : BaseCommand
     {
-                                
+
         public void init(string options)
         {
             try
-            {                
+            {
                 DispatchCommandResult(new PluginResult(PluginResult.Status.OK, ""));
             }
             catch (Exception ex)
@@ -50,17 +50,6 @@ namespace Cordova.Extension.Commands
                 DispatchCommandResult(new PluginResult(PluginResult.Status.ERROR, new { error = "close", message = ex.Message }));
             }
         }
-           
-        private JObject Parse(string options)
-        {
-            string opt = JsonHelper.Deserialize<string[]>(options)[0];
-            return JObject.Parse(opt);
-        }
-
-        private T Parse<T>(string options)
-        {
-            string opt = JsonHelper.Deserialize<string[]>(options)[0];
-            return !String.IsNullOrEmpty(opt) ? JsonConvert.DeserializeObject<T>(opt) : default(T);
-        }
-    }
+    }   
+        
 }
