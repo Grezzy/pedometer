436e938abe8dab0587c285ee8d9514e13f9fb599
artemlakomov <artem.lakomov@gmail.com>
2014-11-14 10:28:59 +0200

    android class added

---
+++ src/android/Pedometer.java
@@ -0,0 +1,25 @@
+package com.grezzy.pedometer;
+
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.PluginResult;
+
+import android.app.Activity;
+import android.content.Context;
+import android.hardware.*;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Pedometer extends CordovaPlugin {
+
+    private SensorManager sensorManager;
+    boolean activityRunning;
+}
